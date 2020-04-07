ad84513ec914abfdd65f7f0b009adcd6e3ea5acc
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-04-07 08:54:10 +0000

    cross

--- Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/12-16Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
+++ Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/12-16Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
@@ -33,155 +33,164 @@ last_mat_timestep=-1
 
 band_start=12
 band_cutoff=16
-session_name=my_parameters.session_name
+# session_name=my_parameters.session_name
+session_file_list=my_parameters.List_FILE
+# print(session_file_list)
 
-if band_start ==0.5:
-    bandwidth_token='0_5'+'-'+ str(band_cutoff) + 'Hz'
+# Cross Sessions Control Start
 
-else:
-    bandwidth_token=str(band_start) +'-'+ str(band_cutoff) + 'Hz'
+for k in range(len(session_file_list)):
+    session_name=str(session_file_list[k])[:-4]
+    print('session_name=', session_name)
+    if band_start ==0.5:
+        bandwidth_token='0_5'+'-'+ str(band_cutoff) + 'Hz'
 
-
-# 'pos' or 'vel'
-kinematic_variable_type=my_parameters.kinematic_variable_type
-
-# nwb file
-nwb_filename = '../../../Dataset/The_nwb_Raw_Dataset/'+session_name+'.nwb'
-nwb_file = h5py.File(nwb_filename, 'r')
-data = nwb_file['/acquisition/timeseries/broadband/data']
-conversion = data.attrs['conversion']
-electrode_map = nwb_file['/general/extracellular_ephys/electrode_map']
-nwb_timestamp = nwb_file['/acquisition/timeseries/broadband/timestamps']
-
-# mat file
-mat_file_name_1='../../../Dataset/Sorted_Spike_Dataset/'+session_name+'.mat'
-mat_file=h5py.File(mat_file_name_1, 'r')
-mat_timestamp=mat_file.get('t')
-mat_timestamp=np.array(mat_timestamp)
-print('YEEE shape of mat_timestamp', mat_timestamp.shape, '\n')
-
-start_second=math.floor(mat_timestamp[0][0])
-last_mat_timestep=mat_timestamp[0][-1]
-end_second=start_second+plot_time_duration
-
-is_first_loop=True # Optimize write file system
-is_last_loop=False
-
-while(is_last_loop==False):
-
-    if end_second>last_mat_timestep:
-        is_last_loop=True
-
-    mat_time_interval=np.where(np.logical_and(mat_timestamp[0,:]>start_second, mat_timestamp[0,:]<end_second ) )
-    # print('mat_timestamp np.where result = ', end='')
-    # print(mat_time_interval, '\n')
-    # print('type of mat_time_interval=', type(mat_time_interval),'\n')
-    # print('mat_time_interval start time index = ', mat_time_interval[0][0],'\n')
-    # print('mat_time_interval end time index = ', mat_time_interval[0][-1],'\n')
-
-    # new timestamp in mat file
-    new_mat_time_stamp=mat_timestamp[0,mat_time_interval[0][0]:mat_time_interval[0][-1]]
-    print('new_mat_time_stamp = ', new_mat_time_stamp,'\n')
+    else:
+        bandwidth_token=str(band_start) +'-'+ str(band_cutoff) + 'Hz'
 
 
-    # Extract time interval from nwb file
-    nwb_time_interval=np.where(np.logical_and(nwb_timestamp[:,]>start_second, nwb_timestamp[:,]<end_second ) )
-    # print('nwb_timestamp np.where result = ', end='')    
-    # print('type of nwb_time_interval = ', type(nwb_time_interval),'\n')
-    # print('nwb_time_interval start time index = ', nwb_time_interval[0][0],'\n')
-    # print('nwb_time_interval end time index = ', nwb_time_interval[0][-1],'\n')
-    new_nwb_time_stamp= nwb_timestamp[nwb_time_interval[0][0]:nwb_time_interval[0][-1],]
-    print('new_nwb_time_stamp = ', new_nwb_time_stamp, '\n')
-    sampling_rate=1/( nwb_timestamp[1,]- nwb_timestamp[0,])
+    # 'pos' or 'vel'
+    kinematic_variable_type=my_parameters.kinematic_variable_type
 
-    channel_1=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+channel_number]
-    channel_2=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+69]
-    channel_3=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+50]
+    # nwb file
+    nwb_filename = '../../../Dataset/The_nwb_Raw_Dataset/'+session_name+'.nwb'
+    nwb_file = h5py.File(nwb_filename, 'r')
+    data = nwb_file['/acquisition/timeseries/broadband/data']
+    conversion = data.attrs['conversion']
+    electrode_map = nwb_file['/general/extracellular_ephys/electrode_map']
+    nwb_timestamp = nwb_file['/acquisition/timeseries/broadband/timestamps']
+
+    # mat file
+    mat_file_name_1='../../../Dataset/Sorted_Spike_Dataset/'+session_name+'.mat'
+    mat_file=h5py.File(mat_file_name_1, 'r')
+    mat_timestamp=mat_file.get('t')
+    mat_timestamp=np.array(mat_timestamp)
+    print('YEEE shape of mat_timestamp', mat_timestamp.shape, '\n')
+
+    start_second=math.floor(mat_timestamp[0][0])
+    last_mat_timestep=mat_timestamp[0][-1]
+    end_second=start_second+plot_time_duration
+
+    is_first_loop=True # Optimize write file system
+    is_last_loop=False
+
+    while(is_last_loop==False):
+
+        if end_second>last_mat_timestep:
+            is_last_loop=True
+
+        mat_time_interval=np.where(np.logical_and(mat_timestamp[0,:]>start_second, mat_timestamp[0,:]<end_second ) )
+        # print('mat_timestamp np.where result = ', end='')
+        # print(mat_time_interval, '\n')
+        # print('type of mat_time_interval=', type(mat_time_interval),'\n')
+        # print('mat_time_interval start time index = ', mat_time_interval[0][0],'\n')
+        # print('mat_time_interval end time index = ', mat_time_interval[0][-1],'\n')
+
+        # new timestamp in mat file
+        new_mat_time_stamp=mat_timestamp[0,mat_time_interval[0][0]:mat_time_interval[0][-1]]
+        print('new_mat_time_stamp = ', new_mat_time_stamp,'\n')
 
 
-    instance_phase_all_channels=[]
-    # good_channel_list_start_from_one=[39,41,76,42,26,29,33,93,21,2,54]
-    for channel_number_yee in range(96):
-    # for channel_number_yee in good_channel_list_start_from_one:
-        # channel_number_yee=channel_number_yee-1
-        channel_1=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+channel_number_yee]
-        # filtered_data_1=buttersworth_filter.butter_highpass_filter(channel_1, band_start, sampling_rate, order=2)
-        # filtered_data_1=buttersworth_filter.butter_lowpass_filter(filtered_data_1, band_cutoff, sampling_rate, order=2)
-        filtered_data_1=buttersworth_filter.butter_bandpass_filter(channel_1, band_start, band_cutoff, sampling_rate, order=2)
-        analytic_signal_1 = hilbert(filtered_data_1)
-        instantaneous_phase = np.angle(analytic_signal_1)
-        instance_phase_all_channels.append(instantaneous_phase)
+        # Extract time interval from nwb file
+        nwb_time_interval=np.where(np.logical_and(nwb_timestamp[:,]>start_second, nwb_timestamp[:,]<end_second ) )
+        # print('nwb_timestamp np.where result = ', end='')    
+        # print('type of nwb_time_interval = ', type(nwb_time_interval),'\n')
+        # print('nwb_time_interval start time index = ', nwb_time_interval[0][0],'\n')
+        # print('nwb_time_interval end time index = ', nwb_time_interval[0][-1],'\n')
+        new_nwb_time_stamp= nwb_timestamp[nwb_time_interval[0][0]:nwb_time_interval[0][-1],]
+        print('new_nwb_time_stamp = ', new_nwb_time_stamp, '\n')
+        sampling_rate=1/( nwb_timestamp[1,]- nwb_timestamp[0,])
 
-    instance_phase_all_channels=np.array(instance_phase_all_channels)
+        channel_1=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+channel_number]
+        channel_2=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+69]
+        channel_3=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+50]
 
-    ITPC_angle=[]
-    ITPC_abs=[]
-    for itpc_loop in range( instance_phase_all_channels.shape[1] ) :
-        itpc_angle=0
-        itpc_abs=0
-        # itpc = abs(mean(exp( i* 1-D_signal_array )))
-        # print('1: ', instance_phase_all_channels[:][itpc_loop:itpc_loop+1],'\n')
-        # print('1j * instance_phase_all_channels[:,itpc_loop]=', 1j * instance_phase_all_channels[:,itpc_loop],'\n')
-        itpc_angle=np.angle( np.mean (np.exp( 1j * instance_phase_all_channels[:,itpc_loop]  )))
-        itpc_abs=np.abs( np.mean (np.exp( 1j * instance_phase_all_channels[:,itpc_loop]  )))
 
-        ITPC_angle.append(itpc_angle)
-        ITPC_abs.append(itpc_abs)
+        instance_phase_all_channels=[]
+        # good_channel_list_start_from_one=[39,41,76,42,26,29,33,93,21,2,54]
+        for channel_number_yee in range(96):
+        # for channel_number_yee in good_channel_list_start_from_one:
+            # channel_number_yee=channel_number_yee-1
+            channel_1=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+channel_number_yee]
+            # filtered_data_1=buttersworth_filter.butter_highpass_filter(channel_1, band_start, sampling_rate, order=2)
+            # filtered_data_1=buttersworth_filter.butter_lowpass_filter(filtered_data_1, band_cutoff, sampling_rate, order=2)
+            filtered_data_1=buttersworth_filter.butter_bandpass_filter(channel_1, band_start, band_cutoff, sampling_rate, order=2)
+            analytic_signal_1 = hilbert(filtered_data_1)
+            instantaneous_phase = np.angle(analytic_signal_1)
+            instance_phase_all_channels.append(instantaneous_phase)
 
-    ITPC_angle=np.array(ITPC_angle).transpose()
-    ITPC_abs=np.array(ITPC_abs).transpose()
+        instance_phase_all_channels=np.array(instance_phase_all_channels)
 
-    print('---'*30)
-    print('ITPC_angle shape= ', ITPC_angle.shape, '\n')
-    print('ITPC_abs shape= ', ITPC_abs.shape, '\n')
+        ITPC_angle=[]
+        ITPC_abs=[]
+        for itpc_loop in range( instance_phase_all_channels.shape[1] ) :
+            itpc_angle=0
+            itpc_abs=0
+            # itpc = abs(mean(exp( i* 1-D_signal_array )))
+            # print('1: ', instance_phase_all_channels[:][itpc_loop:itpc_loop+1],'\n')
+            # print('1j * instance_phase_all_channels[:,itpc_loop]=', 1j * instance_phase_all_channels[:,itpc_loop],'\n')
+            itpc_angle=np.angle( np.mean (np.exp( 1j * instance_phase_all_channels[:,itpc_loop]  )))
+            itpc_abs=np.abs( np.mean (np.exp( 1j * instance_phase_all_channels[:,itpc_loop]  )))
 
-    print('len of new_nwb_time_stamp= ', len(new_nwb_time_stamp), '\n')
-    # Write result to csv
-    CWD = os.getcwd()
-    # CWD= os.path.join('..')
-    if 'Inter-Channel_Clustering_Output_Table' not in CWD:
-        CWD=os.path.join(CWD, 'Inter-Channel_Clustering_Output_Table')
-        if not os.path.exists(CWD):
+            ITPC_angle.append(itpc_angle)
+            ITPC_abs.append(itpc_abs)
+
+        ITPC_angle=np.array(ITPC_angle).transpose()
+        ITPC_abs=np.array(ITPC_abs).transpose()
+
+        print('---'*30)
+        print('ITPC_angle shape= ', ITPC_angle.shape, '\n')
+        print('ITPC_abs shape= ', ITPC_abs.shape, '\n')
+
+        print('len of new_nwb_time_stamp= ', len(new_nwb_time_stamp), '\n')
+        # Write result to csv
+        CWD = os.getcwd()
+        # CWD= os.path.join('..')
+        if 'Inter-Channel_Clustering_Output_Table' not in CWD:
+            CWD=os.path.join(CWD, 'Inter-Channel_Clustering_Output_Table')
+            if not os.path.exists(CWD):
+                    os.mkdir(CWD)
+
+        if session_name not in CWD:
+            CWD=os.path.join(CWD, session_name)
+            if not os.path.exists(CWD):
                 os.mkdir(CWD)
 
-    if session_name not in CWD:
-        CWD=os.path.join(CWD, session_name)
-        if not os.path.exists(CWD):
-            os.mkdir(CWD)
 
+        if bandwidth_token not in CWD:
+            CWD=os.path.join(CWD, bandwidth_token)
+            if not os.path.exists(CWD):
+                    os.mkdir(CWD)
 
-    if bandwidth_token not in CWD:
-        CWD=os.path.join(CWD, bandwidth_token)
-        if not os.path.exists(CWD):
-                os.mkdir(CWD)
+        csv_path=os.path.join(CWD, '24kHz')
+        if not os.path.exists(csv_path):
+            os.mkdir(str(csv_path))
 
-    csv_path=os.path.join(CWD, '24kHz')
-    if not os.path.exists(csv_path):
-        os.mkdir(str(csv_path))
+        print('csv_path= ', csv_path, '\n')
 
-    print('csv_path= ', csv_path, '\n')
+        if is_first_loop==True:
+            is_first_loop=False
+            try:
+                os.remove(os.path.join(csv_path,'24kHz_angle' + '.csv'))
+                os.remove(os.path.join(csv_path,'24kHz_abs' +'.csv'))
+                os.remove(os.path.join(csv_path,'24kHz_nwb_time_stamp.csv'))
+                print('\nOld file deleted\n')
+            except:
+                print('\nNo old files\n')
+        # https://stackoverflow.com/questions/17530542/how-to-add-pandas-data-to-an-existing-csv-file
+        df = pd.DataFrame(ITPC_angle)
+        df.to_csv(os.path.join(csv_path,'24kHz_angle'+ '.csv'), mode='a', index=False, header=False)
 
-    if is_first_loop==True:
-        is_first_loop=False
-        try:
-            os.remove(os.path.join(csv_path,'24kHz_angle' + '.csv'))
-            os.remove(os.path.join(csv_path,'24kHz_abs' +'.csv'))
-            os.remove(os.path.join(csv_path,'24kHz_nwb_time_stamp.csv'))
-            print('\nOld file deleted\n')
-        except:
-            print('\nNo old files\n')
-    # https://stackoverflow.com/questions/17530542/how-to-add-pandas-data-to-an-existing-csv-file
-    df = pd.DataFrame(ITPC_angle)
-    df.to_csv(os.path.join(csv_path,'24kHz_angle'+ '.csv'), mode='a', index=False, header=False)
+        df = pd.DataFrame(ITPC_abs)
+        df.to_csv(os.path.join(csv_path,'24kHz_abs'+ '.csv'), mode='a', index=False, header=False)
 
-    df = pd.DataFrame(ITPC_abs)
-    df.to_csv(os.path.join(csv_path,'24kHz_abs'+ '.csv'), mode='a', index=False, header=False)
+        df = pd.DataFrame(new_nwb_time_stamp)
+        df.to_csv(os.path.join(csv_path,'24kHz_nwb_time_stamp.csv'), mode='a', index=False, header=False)
 
-    df = pd.DataFrame(new_nwb_time_stamp)
-    df.to_csv(os.path.join(csv_path,'24kHz_nwb_time_stamp.csv'), mode='a', index=False, header=False)
+        start_second+=plot_time_duration
+        end_second+=plot_time_duration
 
-    start_second+=plot_time_duration
-    end_second+=plot_time_duration
+    tEnd=time.time()
+    print('Overall processing time: '+ str ( round( (tEnd-tStart)/60 , 3) )+' minutes' )
 
-tEnd=time.time()
-print('Overall processing time: '+ str ( round( (tEnd-tStart)/60 , 3) )+' minutes' )
+# Cross Sessions Control End
