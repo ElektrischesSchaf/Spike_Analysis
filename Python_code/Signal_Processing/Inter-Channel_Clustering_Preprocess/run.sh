1f367d543254e24defd400321c9a3a206607ef93
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-04-07 16:37:37 +0800

    cross sessions

--- Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/run.sh
+++ Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python ./0_5-40Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
-python ./0_5-40Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
+# python ./0_5-40Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
+# python ./0_5-40Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
 python ./0_5-4Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
 python ./0_5-4Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
-python ./4-8Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
-python ./4-8Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
-python ./8-12Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
-python ./8-12Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
+# python ./4-8Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
+# python ./4-8Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
+# python ./8-12Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py
+# python ./8-12Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
--- Python_code/Signal_Processing/Inter_Channel_Module/parameters.py
+++ Python_code/Signal_Processing/Inter_Channel_Module/parameters.py
@@ -1,3 +1,4 @@
+import os
 class my_parameters():
     channel_number=31
     start_second=300
@@ -7,3 +8,12 @@ class my_parameters():
     band_cutoff=40
     session_name='indy_20161007_02'
     kinematic_variable_type='vel'
+
+    # cross sessions
+    FILE_PATH = '../../../Dataset/The_nwb_Raw_Dataset/'
+    List_FILE = os.listdir(FILE_PATH)
+    List_FILE.sort()
+    List_FILE=List_FILE[1:] # indy_20160622_01 can not read
+    GET_FILE = []
+    for FILE_NAME in List_FILE:
+        GET_FILE.append(FILE_PATH + FILE_NAME)
