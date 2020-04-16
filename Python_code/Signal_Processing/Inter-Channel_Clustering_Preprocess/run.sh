d63251f9f8ab0ebb68956878efae667dda637602
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-04-16 07:24:22 +0000

    remove bad sessions

--- Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/run.sh
+++ Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/run.sh
@@ -6,4 +6,6 @@ python ./0_5-4Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py &&
 python ./4-8Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py &&
 python ./4-8Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py &&
 python ./8-12Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py &&
-python ./8-12Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py
+python ./8-12Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py &&
+python ./12-16Hz/Raw_Data_to_nwb_ITPC_csv_96_Channels.py &&
+python ./12-16Hz/nwb_ITPC_csv_to_mat_ITPC_csv_96_Channels.py 
--- Python_code/Signal_Processing/Inter_Channel_Module/parameters.py
+++ Python_code/Signal_Processing/Inter_Channel_Module/parameters.py
@@ -11,9 +11,15 @@ class my_parameters():
 
     # cross sessions
     FILE_PATH = '../../../Dataset/The_nwb_Raw_Dataset/'
-    List_FILE = os.listdir(FILE_PATH)
-    List_FILE.sort()
-    List_FILE=List_FILE[20:] # 3=indy_20160630_01, 7=indy_20160927_04, 16=indy_20161014_04, 19=indy_20161025_04, 29=indy_20170131_02
+    ALL_List_FILE = os.listdir(FILE_PATH)
+    ALL_List_FILE.sort()
+    # List_FILE=List_FILE[:] # 3=indy_20160630_01, 7=indy_20160927_04, 16=indy_20161014_04, 19=indy_20161025_04, 29=indy_20170131_02
+    bad_session_indices=[3, 7, 16, 19, 29]
+    List_FILE=[]
+    for i in range(30):
+        if i not in bad_session_indices:
+            List_FILE.append(ALL_List_FILE[i] )
+
     GET_FILE = []
     for FILE_NAME in List_FILE:
         GET_FILE.append(FILE_PATH + FILE_NAME)
