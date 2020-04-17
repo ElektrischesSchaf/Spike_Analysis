cbc3867f1c227fede541436707e546c31956a57d
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-04-17 10:31:37 +0800

    delete 24kHz file

--- Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/0_5-4Hz/96_channels_phase_clustering.py
+++ Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/0_5-4Hz/96_channels_phase_clustering.py
@@ -23,7 +23,7 @@ import Inter_Channel_Module.buttersworth_filter as buttersworth_filter
 my_parameters=my_parameters.my_parameters()
 buttersworth_filter=buttersworth_filter.butterworth_filter()
 
-
+this_cwd=os.getcwd()
 channel_number=my_parameters.channel_number
 # start_second=my_parameters.start_second
 start_second=-1 # initial
@@ -40,6 +40,9 @@ session_file_list=my_parameters.List_FILE
 # Cross Sessions Control Start
 
 for k in range(len(session_file_list)):
+
+    ########################################################################## Raw to csv
+
     session_name=str(session_file_list[k])[:-4]
     print('session_name=', session_name)
     if band_start ==0.5:
@@ -47,10 +50,6 @@ for k in range(len(session_file_list)):
 
     else:
         bandwidth_token=str(band_start) +'-'+ str(band_cutoff) + 'Hz'
-
-
-    # 'pos' or 'vel'
-    kinematic_variable_type=my_parameters.kinematic_variable_type
 
     # nwb file
     nwb_filename = '../../../Dataset/The_nwb_Raw_Dataset/'+session_name+'.nwb'
@@ -89,13 +88,7 @@ for k in range(len(session_file_list)):
         print('new_nwb_time_stamp = ', new_nwb_time_stamp, '\n')
         sampling_rate=1/( nwb_timestamp[1,]- nwb_timestamp[0,])
 
-        channel_1=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+channel_number]
-        channel_2=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+69]
-        channel_3=data[ nwb_time_interval[0][0]:nwb_time_interval[0][-1], 0+50]
-
-
         instance_phase_all_channels=[]
-        # good_channel_list_start_from_one=[39,41,76,42,26,29,33,93,21,2,54]
         for channel_number_yee in range(96):
         # for channel_number_yee in good_channel_list_start_from_one:
             # channel_number_yee=channel_number_yee-1
@@ -132,10 +125,10 @@ for k in range(len(session_file_list)):
 
         print('len of new_nwb_time_stamp= ', len(new_nwb_time_stamp), '\n')
         # Write result to csv
-        CWD = os.getcwd()
+        CWD = this_cwd
         # CWD= os.path.join('..')
-        if 'Inter-Channel_Clustering_Output_Table' not in CWD:
-            CWD=os.path.join(CWD, 'Inter-Channel_Clustering_Output_Table')
+        if 'Tables' not in CWD:
+            CWD=os.path.join(CWD, 'Tables')
             if not os.path.exists(CWD):
                     os.mkdir(CWD)
 
@@ -180,5 +173,147 @@ for k in range(len(session_file_list)):
 
     tEnd=time.time()
     print('Overall processing time: '+ str ( round( (tEnd-tStart)/60 , 3) )+' minutes' )
+
+    ########################################################################## csv to mat csv
+
+
+    delta= nwb_timestamp[1,]- nwb_timestamp[0,]
+
+
+    High_angle='Tables/'+ session_name +'/'+bandwidth_token+'/24kHz/24kHz_angle.csv'
+    High_abs='Tables/'+ session_name +'/'+bandwidth_token+'/24kHz/24kHz_abs.csv'
+    new_nwb_time_stamp='Tables/'+ session_name +'/'+bandwidth_token+'/24kHz/24kHz_nwb_time_stamp.csv'
+    chunksize = 1e5
+
+    is_first_loop=True # Optimize write file system
+
+    # https://stackoverflow.com/questions/9394803/python-combine-two-for-loops
+    # https://stackoverflow.com/questions/1663807/how-to-iterate-through-two-lists-in-parallel
+    # https://stackoverflow.com/questions/28138392/skip-iterations-in-enumerated-list-object-python
+    # https://kite.com/python/answers/how-to-skip-the-first-element-of-a-for-loop-in-python
+    # https://stackoverflow.com/questions/10079216/skip-first-entry-in-for-loop-in-python
+
+    iterator=enumerate(zip( pd.read_csv(new_nwb_time_stamp, chunksize=chunksize), pd.read_csv(High_angle, chunksize=chunksize), pd.read_csv(High_abs, chunksize=chunksize) ))
+
+    skip=0
+    for nwb_loop_index, (chunk_new_nwb_time_stamp, chunk_High_angle, chunk_High_abs) in iterator:
+
+        chunk_new_nwb_time_stamp=np.array(chunk_new_nwb_time_stamp)
+        chunk_High_angle=np.array(chunk_High_angle)
+        chunk_High_abs=np.array(chunk_High_abs)
+        print('nwb_loop_index = ', nwb_loop_index, '\n')
+
+        for mat_loop_index in range(skip, mat_timestamp.shape[1]):
+
+            target_start=time.time()
+
+            nwb_timestamp_to_mat_timestamp=[]
+            ITPC_abs_250Hz=[]
+            ITPC_angle_250Hz=[]
+            target=mat_timestamp[0][mat_loop_index]
+            print('-'*50, '\ntarget= ', target, '\n')
+            
+            # for _ in range(skip):
+                # next(iterator, None)
+            # for nwb_loop_index, (chunk_new_nwb_time_stamp, chunk_High_angle, chunk_High_abs) in iterator:  
+
+            print('enumerate number= ', nwb_loop_index, '\n')
+            print('first= ', chunk_new_nwb_time_stamp[0], '\n')
+            print('last= ', chunk_new_nwb_time_stamp[-1], '\n')
+
+            if chunk_new_nwb_time_stamp[-1]<target:
+                skip=mat_loop_index
+                break
+
+            # if chunk_new_nwb_time_stamp[0]>target:
+            #     break
+
+            # print('shape of chunk_new_nwb_time_stamp: ', chunk_new_nwb_time_stamp.shape, '\n')
+            chunk_index=np.where( np.logical_and(target>chunk_new_nwb_time_stamp[:,0], target-delta<chunk_new_nwb_time_stamp[:,0] ))
+            # print(type(chunk_index[0]))
+            if chunk_index[0].size==0:
+                # breakpoint() # TODO explain this
+                chunk_index=np.where( target<chunk_new_nwb_time_stamp[:,0] )
+
+            if chunk_index[0].size>0:
+                
+
+                # if chunk_index[0].size>1:
+                #     print(chunk_index[0])
+                #     breakpoint()
+
+                # print('chunk_index= ', chunk_index, ' \nchunk_new_nwb_time_stamp value=', chunk_new_nwb_time_stamp[chunk_index], '\nchunk_High_angle value=',chunk_High_angle[chunk_index], '\nchunk_High_abs value=',chunk_High_abs[chunk_index] ,'\n')
+                
+                nwb_timestamp_to_mat_timestamp.append(target)
+                ITPC_abs_250Hz.append(chunk_High_abs[chunk_index[0][0]])
+                ITPC_angle_250Hz.append( chunk_High_angle[chunk_index[0][0]] )
+
+
+
+                nwb_timestamp_to_mat_timestamp=np.array(nwb_timestamp_to_mat_timestamp).transpose()
+                ITPC_abs_250Hz=np.array(ITPC_abs_250Hz).transpose()
+                ITPC_angle_250Hz=np.array(ITPC_angle_250Hz).transpose()
+
+                # Write result to csv
+                CWD = this_cwd
+                # CWD= os.path.join('..')
+                
+                if 'Tables' not in CWD:
+                    CWD=os.path.join(CWD, 'Tables')
+                    if not os.path.exists(CWD):
+                        os.mkdir(CWD)
+
+                if session_name not in CWD:
+                    CWD=os.path.join(CWD, session_name)
+                    if not os.path.exists(CWD):
+                        os.mkdir(CWD)
+
+                if bandwidth_token not in CWD:
+                    CWD=os.path.join(CWD, bandwidth_token )
+                    if not os.path.exists(CWD):
+                        os.mkdir(CWD)
+
+                csv_path=os.path.join(CWD, '250Hz')
+                if not os.path.exists(csv_path):
+                    os.mkdir(str(csv_path))
+
+                # print('csv_path= ', csv_path, '\n')
+
+                if is_first_loop==True:
+                    is_first_loop=False
+                    try:
+                        os.remove(os.path.join(csv_path,'nwb_timestamp_to_mat_timestamp.csv'))
+                        os.remove(os.path.join(csv_path,'ITPC_abs_250Hz.csv'))
+                        os.remove(os.path.join(csv_path,'ITPC_angle_250Hz.csv'))
+                        print('\nOld file deleted\n')
+                    except:
+                        print('\nNo old files\n')
+
+                df=pd.DataFrame(nwb_timestamp_to_mat_timestamp)
+                df.to_csv(os.path.join(csv_path,'nwb_timestamp_to_mat_timestamp.csv'), mode='a', index=False, header=False)
+
+                df=pd.DataFrame(ITPC_abs_250Hz)
+                df.to_csv(os.path.join(csv_path,'ITPC_abs_250Hz.csv'), mode='a', index=False, header=False)
+
+                df=pd.DataFrame(ITPC_angle_250Hz)
+                df.to_csv(os.path.join(csv_path,'ITPC_angle_250Hz.csv'), mode='a', index=False, header=False)
+
+                print('end one target search\n')
+                target_end=time.time()
+                print('target processing time: '+ str ( round( (target_end-target_start) , 7) )+' seconds' )
+            
+        del chunk_new_nwb_time_stamp
+        del chunk_High_angle
+        del chunk_High_abs
+        gc.collect()
+
+    os.remove(High_angle)
+    os.remove(High_abs)
+    os.remove(new_nwb_time_stamp)
+
+
+    tEnd=time.time()
+    print('Overall processing time: '+ str ( round( (tEnd-tStart)/60 , 3) )+' minutes' )
+
 
 # Cross Sessions Control End
---
+++ Python_code/Signal_Processing/Inter-Channel_Clustering_Preprocess/merge.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./0_5-4Hz/96_channels_phase_clustering.py
