04d8fd5814d72af29ed90d7500d0696d30ef376a
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-05-10 22:20:36 +0800

    sh

--- Python_code/Signal_Processing/local_phase_clustering/merge.sh
+++ Python_code/Signal_Processing/local_phase_clustering/merge.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-python 0_5-4Hz/kernel_size_3/local_phase_clustering.py
+python 0_5-4Hz/kernel_size_3/local_phase_clustering.py &&
+python 0_5-4Hz/kernel_size_5/local_phase_clustering.py
