1134b68ba8d11ddad36f8464e0a4a80cfc72c20d
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-05-16 13:52:28 +0800

    relu+tanh+tanh

--- Python_code/Deep_Learning_Models/GRU_three_stream.py
+++ Python_code/Deep_Learning_Models/GRU_three_stream.py
@@ -60,7 +60,7 @@ class  GRUModel(torch.nn.Module):
         out_conv_average_phase=torch.tanh( self.fc2(out_conv_average_phase) )
         out_conv_average_sync=torch.tanh( self.fc2(out_conv_average_sync) )
 
-        out =torch.tanh( self.fc3(torch.cat((out_spike, out_conv_average_phase, out_conv_average_sync), 2) ) )
+        out = ( self.fc3(torch.cat((out_spike, out_conv_average_phase, out_conv_average_sync), 2) ) )
         out = self.fc4(out)
         out=out.squeeze(0)
 
