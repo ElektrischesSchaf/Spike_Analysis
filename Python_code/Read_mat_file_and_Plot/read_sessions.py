36e862ec42d925b300af1017c7c77c27a63eb18e
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-08-03 21:29:40 +0800

    thesis

--- Python_code/Read_mat_file_and_Plot/read_sessions.py
+++ Python_code/Read_mat_file_and_Plot/read_sessions.py
@@ -14,6 +14,14 @@ duration_across_all_sessions=[]
 units_per_channel_all_sessions=[]
 has_S1_array_all_sessions = []
 original_testing_data_length_all_session = []
+real_units_all_sessions = []
+
+def histc(X, bins):
+    map_to_bins = np.digitize(X,bins)
+    r = np.zeros(bins.shape)
+    for i in map_to_bins:
+        r[i-1] += 1
+    return r
 
 CWD=os.path.join(CWD_origin, 'sessions_data')
 if not os.path.exists(CWD):
@@ -42,6 +50,83 @@ for session_k in range(len(session_file_list)):
         original_testing_data_length = len(time_stamp_64ms[5000:]) - 1 
         original_testing_data_length_all_session.append(original_testing_data_length)
 
+
+        # calcuate units
+        spikes = mat_file['spikes']
+        firing_rate_cell=[[]]            
+
+        print('spikes shape: ', spikes.shape) #  (3, 192) in indy_20160407_02
+        unit_number=spikes.shape[0]
+        
+        # use S1 array data
+        channel_number = spikes.shape[1]
+
+        # don't use S1 array data
+        # channel_number = 96
+
+        actual_channel_number = 96
+
+        time_stamp_64ms=np.asarray(time_stamp_64ms)
+        time_stamp_64ms=time_stamp_64ms.flatten()
+
+        # Making spike counts matrix
+        # Handling M1 array data
+        include_hash_unit = True
+        for channel_index in range(actual_channel_number):
+            for unit_index in range( spikes.shape[0] ):
+                temp_spike_cell=[]
+                temp_spike_cell=mat_file[( spikes[unit_index][channel_index] )][()]
+                temp_spike_cell=np.asarray(temp_spike_cell)
+                temp_spike_cell=temp_spike_cell.flatten()
+
+                if temp_spike_cell.shape[0] != 2 and include_hash_unit==True:
+
+                    yee=histc(temp_spike_cell, time_stamp_64ms)
+                    #print('shape of yee:  ',yee.shape)
+                    firing_rate_cell.append(yee[:-1])
+                    #print('yee: ',yee)
+
+                # else:
+                #     r = np.zeros( len(time_stamp_64ms)-1 )
+                #     firing_rate_cell.append(r)
+
+                firing_rate_cell.append([])
+
+        # Handling S1 array data
+        if channel_number == actual_channel_number*2:
+            print('Has S1')
+            for channel_index in range(actual_channel_number, actual_channel_number*2):
+
+                for unit_index in range( spikes.shape[0] ):
+                    temp_spike_cell=[]
+                    temp_spike_cell=mat_file[( spikes[unit_index][channel_index] )][()]
+                    temp_spike_cell=np.asarray(temp_spike_cell)
+                    temp_spike_cell=temp_spike_cell.flatten()
+
+                    if temp_spike_cell.shape[0] != 2 and include_hash_unit==True:
+
+                        yee=histc(temp_spike_cell, time_stamp_64ms)
+                        #print('shape of yee:  ',yee.shape)
+                        firing_rate_cell.append(yee[:-1])
+                        #print('yee: ',yee)
+
+                    # else:
+                    #     r = np.zeros( len(time_stamp_64ms)-1 )
+                    #     firing_rate_cell.append(r)
+
+                    firing_rate_cell.append([])
+
+        units_have_value = 0
+        firing_rate_final=[] # not[[]]
+        for row_index in range( len( firing_rate_cell) ):   
+            if len(firing_rate_cell[row_index]):
+                firing_rate_final.append( firing_rate_cell[row_index] )
+                units_have_value += 1
+
+        number_of_real_units = len(firing_rate_final)
+        print('number_of_real_units= ', number_of_real_units, '  units_have_value= ', units_have_value, '\n')
+        real_units_all_sessions.append(number_of_real_units)
+
 df = pd.DataFrame({ 'session': [ str(x)[:-4] for x in session_file_list], 'session length':[x for x in duration_across_all_sessions] })
 df.to_csv(os.path.join(CWD, 'duration_across_all_sessions.csv'), index=False, header=True)
 
@@ -53,3 +138,6 @@ df.to_csv(os.path.join(CWD, 'has_S1_array_all_sessions.csv'), index=False, heade
 
 df = pd.DataFrame({ 'session': [ str(x)[:-4] for x in session_file_list], 'original test length':[x for x in original_testing_data_length_all_session] })
 df.to_csv(os.path.join(CWD, 'original_testing_data_length_all_session.csv'), index=False, header=True)
+
+df = pd.DataFrame({ 'session': [ str(x)[:-4] for x in session_file_list], 'units number':[x for x in real_units_all_sessions] })
+df.to_csv(os.path.join(CWD, 'real_units_all_sessions.csv'), index=False, header=True)
