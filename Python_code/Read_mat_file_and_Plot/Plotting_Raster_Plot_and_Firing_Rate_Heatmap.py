2a7abc570935a776372c8ab52a249e9ad81790de
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-08-26 19:25:13 +0800

    thesis

--- Python_code/Read_mat_file_and_Plot/Plotting_Raster_Plot_and_Firing_Rate_Heatmap.py
+++ Python_code/Read_mat_file_and_Plot/Plotting_Raster_Plot_and_Firing_Rate_Heatmap.py
@@ -4,8 +4,10 @@ import h5py
 import os
 import numpy
 import matplotlib.pyplot as plt 
+import matplotlib.ticker as ticker
+import seaborn as sns
 
-path=r'''../../Figures/Spike_Train_Plots/'''
+path=r'''../../Figures/Spike_and_Heatmap/'''
 if not os.path.exists(path):
     os.mkdir(path)
 
@@ -17,7 +19,7 @@ def histc(X, bins):
         r[i-1] += 1
     return r
 
-session_name='indy_20160407_02'
+session_name='indy_20160411_02'
 with h5py.File('../../Dataset/Sorted_Spike_Dataset/'+session_name+'.mat', 'r') as mat_file:
     # <KeysViewHDF5 ['#refs#', 'chan_names', 'cursor_pos', 'finger_pos', 'spikes', 't', 'target_pos', 'wf']>
     print('print all keys: ',end='')
@@ -83,7 +85,7 @@ with h5py.File('../../Dataset/Sorted_Spike_Dataset/'+session_name+'.mat', 'r') a
     time_stamp_64ms=np.asarray(time_stamp_64ms)
     time_stamp_64ms=time_stamp_64ms.flatten()
 
-    for channel_index in range(96):
+    for channel_index in range(0,96):
         for unit_index in range( spikes.shape[0] ):
             temp_spike_cell=[]
             temp_spike_cell=mat_file[( spikes[unit_index][channel_index] )][()]
@@ -131,37 +133,99 @@ with h5py.File('../../Dataset/Sorted_Spike_Dataset/'+session_name+'.mat', 'r') a
                 firing_rate_cell.append([])
 
 
+start_time_bin=100
+end_time_bin=200
+start_plot_time = time_stamp_64ms[start_time_bin]
+end_plot_time = time_stamp_64ms[end_time_bin]
+
 firing_rate_final=[] # not[[]]
 for row_index in range( len( firing_rate_cell) ):   
     if len(firing_rate_cell[row_index]):
         firing_rate_final.append( firing_rate_cell[row_index] )
 
-remove_index=[]
-for i in range( len(m1_raster) ):
-    # print(len(m1_raster[i]))
-    if len(m1_raster[i]) <= 1:
-        remove_index.append(i)
-        # print('hi')
+firing_rate_final=np.array(firing_rate_final)
 
-for i in range(len(remove_index)):
-    m1_raster.pop(i)
-    
-for i in range( len(m1_raster) ):
+# Eliniate empty units
+valid_rows=[]
+for row_idx in range(firing_rate_final.shape[0]):
+    if not np.all( firing_rate_final[row_idx,:] ==0 ):
+        valid_rows.append(row_idx)
+data = firing_rate_final[valid_rows,:]
+
+for rows in m1_raster[:]:
+    if len(rows)==0:
+        m1_raster.remove(rows)
+
+for rows in s1_raster[:]:
+    if len(rows)==0:
+        s1_raster.remove(rows)
+
+for i in range(len(m1_raster)):
     print(len(m1_raster[i]))
 
-# remove_index_s1=[]
-# for i in range( len(s1_raster) ):
-    # if len(s1_raster[i]) <= 1:
-        # remove_index_s1.append(i)
-# for i in range(len(remove_index_s1)):
-    # s1_raster.pop(i)
+print(len(m1_raster), ' ', len(s1_raster), '\n')
 
 my_plot_width = 16
-my_plot_height = 9
-f ,ax = plt.subplots(2,1, gridspec_kw={'height_ratios': [1, 1],  "hspace":0.2 ,"left":0.1, "right":0.9, "top":0.95, "bottom":0.05}, constrained_layout=True , figsize=(my_plot_width, my_plot_height*1.2))
-ax[0].eventplot(m1_raster, linelengths=0.3)
-ax[0].set_xlim([ 100,200 ])
-ax[1].eventplot(s1_raster, linelengths=0.3)
-ax[1].set_xlim([ 100,200 ])
+my_plot_height = 16
+my_fontsize = 30
+
+
+
+# f ,ax = plt.subplots(2,1, gridspec_kw={'height_ratios': [1,1],  "hspace":0.2 ,"left":0.1, "right":0.9, "top":0.95, "bottom":0.05}, constrained_layout=True , figsize=(my_plot_width, my_plot_height*2))
+
+plt.figure( figsize=(my_plot_width, my_plot_height) )
+cbar_kws={"orientation": "horizontal", "shrink": 0.5, "aspect":50, "use_gridspec":"True", "fraction":0.01 , "pad":0.03, 'ticks' : [ 0, 4 ]}
+ax = sns.heatmap( data=data, vmax=4 , xticklabels=False, yticklabels=True, cbar=False, cmap='YlGnBu_r') # important, not ax[0] = sns.heatmap(...)
+# ax[0].set_xticklabels(ax[0].get_xmajorticklabels(), fontsize = my_fontsize, rotation=0)
+ax.set_title('Firing rate from session '+ session_name, fontsize=my_fontsize)
+
+
+ax.set_yticklabels(ax.get_ymajorticklabels(), fontsize = my_fontsize)
+
+ax.yaxis.set_major_locator(ticker.MultipleLocator(50))
+ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+
+ax.set_xlim([ start_time_bin, end_time_bin ])
+ax.set_xlabel('Time Bins', fontsize=my_fontsize, color="black")
+ax.set_ylabel('Units', fontsize=my_fontsize, color="black")
+
+plt.tight_layout()
+
 # plt.show()
-plt.savefig('yee.png')
+plt.savefig(path+'/'+'s1_and_m1_spike_train.png')
+
+plt.cla()
+plt.clf()
+plt.close()
+
+
+f ,ax = plt.subplots(2,1, gridspec_kw={'height_ratios': [1, 1],  "hspace":0.2 ,"left":0.1, "right":0.9, "top":0.95, "bottom":0.05}, constrained_layout=True , figsize=(my_plot_width, my_plot_height))
+
+ax[0].set_title('M1 spike train', fontsize=my_fontsize)
+ax[0].eventplot(m1_raster, linelengths=1 , color='black')
+ax[0].set_xlim([ start_plot_time, end_plot_time ])
+
+ax[0].yaxis.set_major_locator(ticker.MultipleLocator(50))
+ax[0].yaxis.set_major_formatter(ticker.ScalarFormatter())
+ax[0].get_xaxis().set_ticks([])
+ax[0].set_ylabel('Units', fontsize=my_fontsize, color="black")
+ax[0].tick_params(axis='y', labelsize= my_fontsize*0.5)
+
+
+ax[1].set_title('S1 spike train', fontsize=my_fontsize)
+ax[1].eventplot(s1_raster, linelengths=1 , color='black' )
+ax[1].set_xlim([ start_plot_time, end_plot_time ])
+ax[1].set_ylabel('Units', fontsize=my_fontsize, color="black")
+ax[1].tick_params(axis='x', labelsize= my_fontsize*0.5)
+ax[1].tick_params(axis='y', labelsize= my_fontsize*0.5)
+ax[1].yaxis.set_major_locator(ticker.MultipleLocator(50))
+ax[1].yaxis.set_major_formatter(ticker.ScalarFormatter())
+ax[1].set_xlabel('Time (seconds)', fontsize=my_fontsize )
+
+# plt.show()
+plt.savefig(path+'/'+'the_result_heatmap.png')
+
+plt.cla()
+plt.clf()
+plt.close()
+
