2797dc377d1485d8e8a52586cc6f159d505ae6c9
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-08-29 14:54:10 +0800

    update GRU and GRUcell

---
+++ Python_code/Pytorch/Firing_Rate/GRU/run_all.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+python single_37_sess_spike_GRU_pos.py &&
+python single_37_sess_spike_GRU_vel.py &&
+python single_37_sess_spike_GRU_acc.py
---
+++ Python_code/Pytorch/Firing_Rate/GRU/run_vel_and_acc.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+python single_37_sess_spike_GRU_vel.py &&
+python single_37_sess_spike_GRU_acc.py
