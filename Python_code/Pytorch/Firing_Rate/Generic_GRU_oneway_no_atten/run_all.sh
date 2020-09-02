1ac5eb01592f0b08c00b6ce39e6c7c09f30555b6
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-09-02 13:29:26 +0000

    Bland GRU

---
+++ Python_code/Pytorch/Firing_Rate/Generic_GRU_oneway_no_atten/run_all.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+python single_37_sess_spike_GRU_pos_one_way_real_LN.py &&
+python single_37_sess_spike_GRU_vel_one_way_real_LN.py &&
+python single_37_sess_spike_GRU_acc_one_way_real_LN.py
