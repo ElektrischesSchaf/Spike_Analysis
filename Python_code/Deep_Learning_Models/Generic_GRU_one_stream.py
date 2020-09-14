ba6133814bd7735d22d8e6bec9a818fdb8d1db8a
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-09-14 03:26:49 +0000

    Generic bidir GRU

--- Python_code/Deep_Learning_Models/Generic_GRU_one_stream.py
+++ Python_code/Deep_Learning_Models/Generic_GRU_one_stream.py
@@ -50,3 +50,48 @@ class  GRUModel(torch.nn.Module):
 
 
         return out
+
+
+class  BidirGRU(torch.nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, max_timestep, layer_dim, output_dim):
+        super(BidirGRU, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+        self.input_dim=input_dim
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.GRU = torch.nn.GRU(input_dim, hidden_dim, layer_dim, batch_first=True, bidirectional=True)
+        
+        # Readout layer
+        self.fc1 = torch.nn.Linear( 2*hidden_dim, int(hidden_dim/2) ) # one-directional
+        self.fc2 = torch.nn.Linear( int(hidden_dim/2), output_dim ) # one-directional
+
+        # self.fc1 = torch.nn.Linear(hidden_dim*2, hidden_dim) # bidirectional
+        # self.fc2 = torch.nn.Linear(hidden_dim, output_dim) # bidirectional
+    
+    def forward(self, x):
+
+        # x torch.Size([batch size, feature num * orders])
+        # print('x size 1= ', x.size())
+        x=x.view(x.size(0), -1, self.input_dim)
+        # print('x size 2= ', x.size())
+        # m=torch.nn.LayerNorm( x.size()[:], elementwise_affine=False )
+        # x=m(x)
+
+        # print('input dim= ', x.size(), '\n') # input dim=  torch.Size([1, 64, 96]) => batch_first=True, (batch_dim, seq_dim, feature_dim)
+        # print('yee shape of x= ', x.size())
+        # time steps
+        # print('real input shape= ', x.size(), '\n')
+        out, _ = self.GRU(x)
+        # print('out size 1= ', out.size())
+        out = torch.relu(self.fc1(out[:,-1,:] ))
+        # print('out size 2= ', out.size())
+        out = self.fc2(out)
+        # print('out size 3= ', out.size())
+
+
+        return out
