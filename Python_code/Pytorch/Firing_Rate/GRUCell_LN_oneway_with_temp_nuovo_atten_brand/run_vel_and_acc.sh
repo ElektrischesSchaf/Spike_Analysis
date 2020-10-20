e8f222688450b708cbd6ee5ad24f5c3b290f6925
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-10-20 08:42:56 +0000

    rename

--- Python_code/Pytorch/Firing_Rate/GRUCell_LN_oneway_with_temp_nuovo_atten_brand/run_vel_and_acc.sh
+++ Python_code/Pytorch/Firing_Rate/GRUCell_LN_oneway_with_temp_nuovo_atten_brand/run_vel_and_acc.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-python single_GRU_vel_one_way_real_LN_nuovo_atten.py &&
-python single_GRU_acc_one_way_real_LN_nuovo_atten.py
+python single_GRU_vel_one_way_real_LN_nuovo_atten_brand.py &&
+python single_GRU_acc_one_way_real_LN_nuovo_atten_brand.py
