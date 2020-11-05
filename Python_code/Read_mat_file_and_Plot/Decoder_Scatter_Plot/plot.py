f0fe567671dabc32601b84e027010c1141b673b1
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-11-05 12:13:15 +0800

    mod fig

--- Python_code/Read_mat_file_and_Plot/Decoder_Scatter_Plot/plot.py
+++ Python_code/Read_mat_file_and_Plot/Decoder_Scatter_Plot/plot.py
@@ -8,54 +8,60 @@ from sklearn.linear_model import LinearRegression
 
 model = LinearRegression(fit_intercept=True, normalize=True, copy_X=True)
 
+
 CWD = os.getcwd()
 plot_path = os.path.join(CWD, 'My_plot_bidir_vs_makin')
 if not os.path.exists(plot_path):
     os.mkdir(plot_path)
 
-my_model_name = 'atten_LN_bidir'
+my_model_name_for_y = 'atten_LN_bidir'
 makin_model_name = 'rEFH_dynamic'
 my_fontsize=35
 
-x_pos = pd.read_csv( my_model_name+'/pos.csv', usecols=["x-axis"] , dtype=float)
-x_pos = x_pos.values
-print(x_pos.shape)
+def read_my_result_1_for_y_scatter( my_model_name_for_y ):
+    my_model_name = my_model_name_for_y
+    x_pos = pd.read_csv( my_model_name+'/pos.csv', usecols=["x-axis"] , dtype=float)
+    x_pos = x_pos.values
 
-y_pos = pd.read_csv( my_model_name+'/pos.csv', usecols=["y-axis"] , dtype=float)
-y_pos = y_pos.values
+    y_pos = pd.read_csv( my_model_name+'/pos.csv', usecols=["y-axis"] , dtype=float)
+    y_pos = y_pos.values
 
-x_vel = pd.read_csv( my_model_name+'/vel.csv', usecols=["x-axis"] , dtype=float)
-x_vel = x_vel.values
+    x_vel = pd.read_csv( my_model_name+'/vel.csv', usecols=["x-axis"] , dtype=float)
+    x_vel = x_vel.values
 
-y_vel = pd.read_csv( my_model_name+'/vel.csv', usecols=["y-axis"] , dtype=float)
-y_vel = y_vel.values
+    y_vel = pd.read_csv( my_model_name+'/vel.csv', usecols=["y-axis"] , dtype=float)
+    y_vel = y_vel.values
 
-x_acc = pd.read_csv( my_model_name+'/acc.csv', usecols=["x-axis"] , dtype=float)
-x_acc = x_acc.values
+    x_acc = pd.read_csv( my_model_name+'/acc.csv', usecols=["x-axis"] , dtype=float)
+    x_acc = x_acc.values
 
-y_acc = pd.read_csv( my_model_name+'/acc.csv', usecols=["y-axis"] , dtype=float)
-y_acc = y_acc.values
+    y_acc = pd.read_csv( my_model_name+'/acc.csv', usecols=["y-axis"] , dtype=float)
+    y_acc = y_acc.values
+    return x_pos, y_pos, x_vel, y_vel, x_acc, y_acc
 
-rEFH_x_pos = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["x-pos"] , dtype=float)
-rEFH_x_pos = rEFH_x_pos.values
+def read_makin_rEFH_result(makin_model_name):
+    makin_model_name=makin_model_name
+    rEFH_x_pos = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["x-pos"] , dtype=float)
+    rEFH_x_pos = rEFH_x_pos.values
 
-rEFH_y_pos = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["y-pos"] , dtype=float)
-rEFH_y_pos = rEFH_y_pos.values
+    rEFH_y_pos = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["y-pos"] , dtype=float)
+    rEFH_y_pos = rEFH_y_pos.values
 
-rEFH_x_vel = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["x-vel"] , dtype=float)
-rEFH_x_vel = rEFH_x_vel.values
+    rEFH_x_vel = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["x-vel"] , dtype=float)
+    rEFH_x_vel = rEFH_x_vel.values
 
-rEFH_y_vel = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["y-vel"] , dtype=float)
-rEFH_y_vel = rEFH_y_vel.values
+    rEFH_y_vel = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["y-vel"] , dtype=float)
+    rEFH_y_vel = rEFH_y_vel.values
 
-rEFH_x_acc = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["x-acc"] , dtype=float)
-rEFH_x_acc = rEFH_x_acc.values
+    rEFH_x_acc = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["x-acc"] , dtype=float)
+    rEFH_x_acc = rEFH_x_acc.values
 
-rEFH_y_acc = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["y-acc"] , dtype=float)
-rEFH_y_acc = rEFH_y_acc.values
+    rEFH_y_acc = pd.read_csv( makin_model_name + '/all_SNR.csv', usecols=["y-acc"] , dtype=float)
+    rEFH_y_acc = rEFH_y_acc.values
 
+    return rEFH_x_pos, rEFH_y_pos, rEFH_x_vel, rEFH_y_vel, rEFH_x_acc, rEFH_y_acc
 
-def drawing( rEFH_dynamic_result, my_model_result, axis_type, kinematic_type ):
+def drawing_with_makin( rEFH_dynamic_result, my_model_result, axis_type, kinematic_type , my_model_name_for_y):
     plt.figure(figsize=(12,12))
     plt.scatter( rEFH_dynamic_result[:37], my_model_result[:37], s=100, color='blue', label='Indy' )
     plt.scatter( rEFH_dynamic_result[37:], my_model_result[37:], s=100, color='green' , label='Loco ')
@@ -66,7 +72,7 @@ def drawing( rEFH_dynamic_result, my_model_result, axis_type, kinematic_type ):
 
     if makin_model_name == 'rEFH_dynamic':
         plt.xlabel('SNR(dB), rEFH(+KF)',fontsize=my_fontsize)
-    if my_model_name == 'atten_LN_bidir':
+    if my_model_name_for_y == 'atten_LN_bidir':
         plt.ylabel('SNR(dB), Bidir GRU (+LN & Atten.)',fontsize=my_fontsize)
 
     plt.xticks(fontsize=my_fontsize)
@@ -105,9 +111,14 @@ def drawing( rEFH_dynamic_result, my_model_result, axis_type, kinematic_type ):
     plt.savefig( plot_path + '/' + yee_1 + yee_2 +'.png' )
 
 
-drawing(rEFH_x_pos, x_pos, 'x', 'pos')
-drawing(rEFH_y_pos, y_pos, 'y', 'pos')
-drawing(rEFH_x_vel, x_vel, 'x', 'vel')
-drawing(rEFH_y_vel, y_vel, 'y', 'vel')
-drawing(rEFH_x_acc, x_acc, 'x', 'acc')
-drawing(rEFH_y_acc, y_acc, 'y', 'acc')
+x_pos_1, y_pos_1, x_vel_1, y_vel_1, x_acc_1, y_acc_1 = read_my_result_1_for_y_scatter(my_model_name_for_y)
+
+rEFH_x_pos, rEFH_y_pos, rEFH_x_vel, rEFH_y_vel, rEFH_x_acc, rEFH_y_acc = read_makin_rEFH_result(makin_model_name)
+
+
+drawing_with_makin(rEFH_x_pos, x_pos_1, 'x', 'pos',my_model_name_for_y)
+drawing_with_makin(rEFH_y_pos, y_pos_1, 'y', 'pos',my_model_name_for_y)
+drawing_with_makin(rEFH_x_vel, x_vel_1, 'x', 'vel',my_model_name_for_y)
+drawing_with_makin(rEFH_y_vel, y_vel_1, 'y', 'vel',my_model_name_for_y)
+drawing_with_makin(rEFH_x_acc, x_acc_1, 'x', 'acc',my_model_name_for_y)
+drawing_with_makin(rEFH_y_acc, y_acc_1, 'y', 'acc',my_model_name_for_y)
