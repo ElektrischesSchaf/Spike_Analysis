4b141f41ccd2bdf003e51c473f9d717554430f37
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-12-07 07:57:19 +0000

    eval

--- Python_code/data_processing/some_modules.py
+++ Python_code/data_processing/some_modules.py
@@ -204,9 +204,9 @@ class regular_modules():
 
     def evlauate_performance( self, real_y, predicted_y, max_timestep ):
         testing_data_r_square = r2_score( real_y[max_timestep:] , predicted_y[max_timestep:] )
-        testing_data_SNR = -10*math.log10(1-testing_data_r_square)
-        testing_data_RMSE = np.sqrt(mean_squared_error(real_y, predicted_y))
-        PCC = pearsonr(real_y, predicted_y)
+        testing_data_SNR = -10*math.log10( 1-testing_data_r_square )
+        testing_data_RMSE = np.sqrt(mean_squared_error( real_y[max_timestep:], predicted_y[max_timestep:] ))
+        PCC = pearsonr( real_y[max_timestep:] , predicted_y[max_timestep:] )
 
         return testing_data_r_square, testing_data_SNR, testing_data_RMSE, PCC
 
