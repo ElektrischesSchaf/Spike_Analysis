445f13d870ff8f06c14e1a09a5e8d705a5f764ed
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2020-12-13 20:25:29 +0800

    thesis

--- Python_code/Read_mat_file_and_Plot/penalization_term_illustration/plot.py
+++ Python_code/Read_mat_file_and_Plot/penalization_term_illustration/plot.py
@@ -13,8 +13,12 @@ A_transpose = np.transpose(A)
 df = pd.DataFrame( A_transpose )
 df.to_csv( 'A_transpose.csv', index=False, header=False)
 
-result = np.matmul(A,A_transpose) - np.eye( A.shape[0] , dtype=float )
+my_identity_matrix = np.eye( A.shape[0] , dtype=float )
+
+df = pd.DataFrame( my_identity_matrix )
+df.to_csv( 'identity_matrix.csv', index=False, header=False)
+
+result = np.matmul(A,A_transpose) - my_identity_matrix
 
 df = pd.DataFrame( result )
 df.to_csv( 'result.csv', index=False, header=False)
-
